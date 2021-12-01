c38c5ad24f09670e8f27c42dc4fd2ddbad12393a
shingogoto8210 <chupps8210@gmail.com>
2021-12-01 12:54:16 +0900

    [Add] DOPathでステージを自動で移動する機能実装（停止・移動再開機能まで）

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private RailMoveController railMoveController;
+
+    [SerializeField]
+    private RailPathData railPathData;
+
+    public GameState currentGameState;
+
+    void Start()
+    {
+        //ゲームの状態（準備中）
+        currentGameState = GameState.Wait;
+        Debug.Log("準備中");
+
+        //railMoveControllerの設定
+        railMoveController.SetUpRailMoveController(this);
+
+        //ゲームの状態（プレイ中）
+        currentGameState = GameState.Play;
+        Debug.Log("ゲーム開始");
+
+        //移動開始
+        railMoveController.Move();
+
+        //プレイ中は移動と停止操作可能
+        StartCoroutine(railMoveController.ChangeMove());
+    }
+
+    //DOPathが最後まで行ったら，次のRailPathDataがあるか確認する。
+    public void CheckNextRailPathData()
+    {
+        //TODO 次のRailPathDataがあれば，そちらに進む
+
+        //次のRailPathDataがなければゲーム終了
+        currentGameState = GameState.GameOver;
+        
+        //tweenをKill
+        railMoveController.tween.Kill();
+
+        Debug.Log("ゲーム終了");
+
+    }
+}
---
+++ Assets/Scripts/RailMoveController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq;
+
+public class RailMoveController : MonoBehaviour
+{
+    public Tween tween;
+
+    [SerializeField]
+    private RailPathData railPathData;
+
+    private Vector3[] paths;
+
+    [SerializeField, Header("移動時間")]
+    private float moveTime;
+
+    private bool isStop;
+
+    private GameManager gameManager;
+
+    /// <summary>
+    /// RailMoveControllerの設定
+    /// </summary>
+    /// <param name="gameManager"></param>
+    public void SetUpRailMoveController(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
+    /// <summary>
+    /// 移動する
+    /// </summary>
+    public void Move()
+    {
+        //railPathDataからTransform型の配列を取得し，Vector３型に直して変数に代入
+        paths = railPathData.railPathDatas.Select(x => x.position).ToArray();
+
+        //Pathデータを順に進んでいき，最後のところで続きあるか確認。なければ終了。
+        tween = transform.DOPath(paths, moveTime).SetEase(Ease.Linear).OnComplete(() => gameManager.CheckNextRailPathData());
+        Debug.Log("移動開始");
+    }
+
+    /// <summary>
+    /// 止まる
+    /// </summary>
+    public void Stop()
+    {
+        tween.Pause();
+        Debug.Log("停止");
+    }
+
+    /// <summary>
+    /// 再び移動する
+    /// </summary>
+    public void Resume()
+    {
+        tween.Play();
+        Debug.Log("移動再開");
+    }
+    
+    //移動と停止を変更する
+    public IEnumerator ChangeMove()
+    {
+        while (gameManager.currentGameState == GameState.Play)
+        {
+            //移動中にスペースを押すと止まる
+            if (isStop == false && Input.GetKeyDown(KeyCode.Space))
+            {
+                Stop();
+                isStop = true;
+            }
+            //停止中にスペースを押すと移動再開
+            else if (isStop && Input.GetKeyDown(KeyCode.Space))
+            {
+                Resume();
+                isStop = false;
+            }
+            yield return null;
+        }
+    }
+}
