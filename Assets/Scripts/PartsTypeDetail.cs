2ab5d584fe0ea5ef083a5f0c7582686c0c814a4c
shingogoto8210 <chupps8210@gmail.com>
2021-12-22 10:33:00 +0900

    [refine]Weapon.csとPartsTypeDetail.csをリファクタリング

--- Assets/Scripts/PartsTypeDetail.cs
+++ Assets/Scripts/PartsTypeDetail.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class PartsTypeDetail : MonoBehaviour
 {
     public PartsType partsType;
+
+    public void HitCheckParts(GameManager gameManager)
+    {
+        if (this.partsType == PartsType.head)
+        {
+            CapsuleCollider enemyCol = this.GetComponent<CapsuleCollider>();
+            enemyCol.enabled = false;
+            EnemyController enemy = this.transform.root.gameObject.GetComponent<EnemyController>();
+            StartCoroutine(enemy.DestroyEnemy(gameManager, enemy.point * 5));
+            Debug.Log("headŽËŒ‚");
+
+        }
+        else if (this.partsType == PartsType.body)
+        {
+            EnemyController enemyController = this.transform.gameObject.GetComponent<EnemyController>();
+            StartCoroutine(enemyController.DestroyEnemy(gameManager, enemyController.point));
+            Debug.Log("bodyŽËŒ‚");
+        }
+    }
 }
